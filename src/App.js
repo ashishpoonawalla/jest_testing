0c717f7f1a5cea0589e8e8cf9a4f33f39f8673ca
Ashish <ashish.omar.pune@gmail.com>
2022-11-01 12:00:31 +0530

    Add Wallaby

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App()
+{
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +18,11 @@ function App() {
         >
           Learn React
         </a>
+        <ul>
+          <li>Apple</li>
+          <li>Banana</li>
+          <li>Orange</li>
+        </ul>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', () =>
+{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('renders 3 list items', () =>
+{
+  render(<App />);
+  const listItems = screen.getAllByRole("listitem");
+  expect(listItems.length).toEqual(5);
 });
